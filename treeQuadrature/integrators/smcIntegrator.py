fbc67b2d41cf12fb1ec2d3f9647579a821459938
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2024-07-17 21:59:41 +0100

    added return_std option to SmcIntegrator

--- treeQuadrature/integrators/smcIntegrator.py
+++ treeQuadrature/integrators/smcIntegrator.py
@@ -16,7 +16,8 @@ class SmcIntegrator(Integrator):
     def __init__(self, N: int):
         self.N = N
 
-    def __call__(self, problem: Problem, return_N: bool=False):
+    def __call__(self, problem: Problem, return_N: bool=False, 
+                 return_std: bool=False):
         """
         Perform the integration process.
 
@@ -26,21 +27,27 @@ class SmcIntegrator(Integrator):
             The integration problem
         return_N : bool, optional
             If True, return the number of samples used.
+        return_std : bool, optional
+            If True, return the standard deviation of the Monte Carlo estimate.
 
         Return
         -------
         dict
             with the following keys:
             - 'estimate' (float) : estimated integral value
-            - 'n_evals' (int) :  number of function estiamtions
+            - 'n_evals' (int) :  number of function estiamtions, if return_N is True
+            - 'std' (float) : standard deviation of the estimate, if return_std is True
         """
         # Draw N samples from the prior distribution
         xs = problem.p.rvs(self.N)
         # Evaluate the likelihood at these samples
         ys = problem.d.pdf(xs).reshape(-1)
         G = np.mean(ys)
+        std_G = np.std(ys) / np.sqrt(self.N)  # Standard deviation of the mean
 
         ret = {'estimate': G}
         if return_N:
             ret['n_evals'] = self.N
+        if return_std:
+            ret['std'] = std_G
         return ret

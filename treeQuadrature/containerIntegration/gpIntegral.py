6bcaeda9ebfeb92a6b506017ad969d5a6660652d
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2024-07-20 18:58:02 +0100

    changed RbfIntegral to return length_scale (opt)

--- treeQuadrature/containerIntegration/gpIntegral.py
+++ treeQuadrature/containerIntegration/gpIntegral.py
@@ -1,8 +1,9 @@
-from typing import Dict, Any
+from typing import Dict, Any, Callable
 from sklearn.gaussian_process.kernels import RBF
 
 from ..gaussianProcess import fit_GP, GP_diagnosis, rbf_Integration
 from .containerIntegral import ContainerIntegral
+from ..container import Container
 
 class RbfIntegral(ContainerIntegral):
     """
@@ -15,6 +16,10 @@ class RbfIntegral(ContainerIntegral):
     length : float
         the initial value of the length scale of RBF kernel
         default 1.0
+    range : float
+        GPRegressor will search hyper-parameter
+        among (length * 1/range, length * range)
+        Default 1e2
     const : float
         the initial value of the constant kerenl
         default 1.0
@@ -45,6 +50,7 @@ class RbfIntegral(ContainerIntegral):
         
         self.options : Dict[str, Any] = {
             'length': 10,
+            'range': 1e2,
             'n_samples': 15,
             'n_tuning': 10,
             'factr': 1e7,
@@ -64,22 +70,43 @@ class RbfIntegral(ContainerIntegral):
 
     @staticmethod
     def _validate_options(options):
-        if not isinstance(options['length'], int):
-            raise TypeError('length must be an int')
+        if not isinstance(options['length'], (int, float)):
+            raise TypeError(f'length must be an int or float, got {options['length']}')
         if not isinstance(options['n_samples'], int):
-            raise TypeError('n_samples must be an int')
+            raise TypeError(f'n_samples must be an int, got {options['n_samples']}')
         if not isinstance(options['n_tuning'], int):
-            raise TypeError('n_tuning must be an int')
+            raise TypeError(f'n_tuning must be an int, got {options['n_tuning']}')
         if not isinstance(options['factr'], (int, float)):
-            raise TypeError('factr must be an int or float')
+            raise TypeError(f'factr must be an int or float, got {options['factr']}')
         if not isinstance(options['max_iter'], (int, float)):
-            raise TypeError('max_iter must be an int or float')
+            raise TypeError(f'max_iter must be an int or float, got {options['max_iter']}')
         if not isinstance(options['check_GP'], bool):
             raise TypeError('check_GP must be a bool')
         if not isinstance(options['return_std'], bool):
             raise TypeError('return_std must be a bool')
 
-    def containerIntegral(self, container, f, **kwargs: Any):
+    def containerIntegral(self, container: Container, f: Callable, return_hyper_params: bool = False, 
+                          **kwargs: Any):
+        """
+        Arguments
+        ---------
+        container: Container
+            the container on which the integral of f should be evaluated
+        f : function
+            takes X : np.ndarray and return np.ndarray, 
+            see pdf method of Distribution class in exampleDistributions.py
+        return_hyper_params : bool
+            if True, 
+        kwargs : Any
+            other arguments allowed (see RbfIntegral attributes)
+        
+        Return
+        ------
+        float or tuple
+            value of the integral of f on the container, 
+            and std if return_std = True, 
+            and hyper_parameters of GP fitting if 
+        """
         options = self.options.copy()
         options.update(kwargs)
 
@@ -90,21 +117,12 @@ class RbfIntegral(ContainerIntegral):
             setattr(self, key, value)
 
         # redraw uniform samples from the container
-        # TODO -keep adjusting the number of samples
-        # if container.volume > 0.05:
-        #     xs = container.rvs(self.n_samples * 8)
-        # elif container.volume > 0.01:
-        #     xs = container.rvs(self.n_samples * 2)
-        # elif container.volume > 0.025:
-        #     xs = container.rvs(self.n_samples * 4)
-        # else:
-        #     xs = container.rvs(self.n_samples)
         xs = container.rvs(self.n_samples)
         ys = f(xs)
         container.add(xs, ys)  # for tracking num function evaluations
 
         # fit GP using RBF kernel
-        kernel = RBF(self.length, (self.length*1e-2, self.length*1e2))
+        kernel = RBF(self.length, (self.length*(1/self.range), self.length*self.range))
         gp = fit_GP(xs, ys, kernel, self.n_tuning, self.max_iter, self.factr)
 
         ### GP diagnosis
@@ -113,4 +131,10 @@ class RbfIntegral(ContainerIntegral):
             GP_diagnosis(gp, xs, ys, container, 
                     criterion = lambda container : container.volume > 0.05)
         
-        return rbf_Integration(gp, container, xs, self.return_std)
+        integral_result = rbf_Integration(gp, container, xs, self.return_std)
+        
+        if return_hyper_params:
+            hyper_params = {'length' : gp.kernel_.length_scale}
+            return integral_result, hyper_params
+        else:
+            return integral_result

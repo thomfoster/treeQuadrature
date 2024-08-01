c0100993b9ee1cc0002217f798b9c936f767abe4
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2024-08-01 13:47:00 +0100

    change local_single_container_GP to multiple runs

--- examples/local_single_container_GP.py
+++ examples/local_single_container_GP.py
@@ -1,4 +1,4 @@
-import time
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -9,32 +9,38 @@ from treeQuadrature.exampleProblems import SimpleGaussian
 from treeQuadrature.container import Container
 
 Ds = range(1, 14)
-
 N = 8_000
 P = 40
-
+num_runs = 10
 
 integ = SimpleIntegrator(N, P, MinSseSplit(), RbfIntegral())
 
-lengths = []
+lengths = np.zeros((num_runs, len(Ds)))
 
-for D in Ds:
-    print(f'D={D}')
-    problem = SimpleGaussian(D)
-    X = integ.sampler.rvs(N, problem)
-    y = problem.integrand(X)
-    root = Container(X, y, mins=problem.lows, maxs=problem.highs)
+for i in range(num_runs):
+    for j, D in enumerate(Ds):
+        print(f'Run {i+1}, D={D}')
+        problem = SimpleGaussian(D)
+        X = integ.sampler.rvs(N, problem)
+        y = problem.integrand(X)
+        root = Container(X, y, mins=problem.lows, maxs=problem.highs)
 
-    containers = integ.construct_tree(root)
+        containers = integ.construct_tree(root)
 
-    n_cont = len(containers)
+        n_cont = len(containers)
+        lengths[i, j] = n_cont
 
-    lengths.append(n_cont)
+# Calculate mean and standard deviation across the runs
+mean_lengths = np.mean(lengths, axis=0)
+std_lengths = np.std(lengths, axis=0)
 
-plt.plot(Ds, lengths)
-plt.title('Number of containers vs dimension of problem')
+# Plotting
+plt.plot(Ds, mean_lengths, label='Mean Number of Containers')
+plt.fill_between(Ds, mean_lengths - std_lengths, mean_lengths + std_lengths, alpha=0.2, label='Std Dev')
+plt.title('Number of Containers vs Dimension of Problem')
 plt.xlabel('Dimension')
-plt.ylabel('Number')
+plt.ylabel('Number of Containers')
+plt.legend()
 
 # Display the plot
 plt.show()

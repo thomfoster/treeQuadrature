47fe7c11c177daedafe7d2cbf1ae6149ba128665
Daniel Lin <daniel.kansaki@outlook.com>
2024-08-05 17:10:53 +0100

    modified local_gp_errors

--- examples/local_gp_errors.py
+++ examples/local_gp_errors.py
@@ -6,6 +6,9 @@ from treeQuadrature.splits import MinSseSplit
 from treeQuadrature.visualisation import plotContainers, plotIntegrand
 
 import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
@@ -20,13 +23,15 @@ if __name__ == '__main__':
 
     Ds = np.arange(2, 12, 2)
     for D in Ds:
+        # problem = QuadraticProblem(D=D)
         # problem = Gaussian(D=D, lows=-1.0, highs=1.0, Sigma=1/200)
-        # problem = ExponentialProductProblem(D=D)
-        problem = QuadraticProblem(D=D)
+        problem = ExponentialProductProblem(D=D)
 
-        if problem.D == 2:
-            plotIntegrand(problem.integrand, D, xlim=[problem.lows[0], problem.highs[0]], 
-                          ylim=[problem.lows[1], problem.highs[1]])
+        print(f'solving {str(problem)}')
+
+        # if problem.D == 2:
+        #     plotIntegrand(problem.integrand, D, xlim=[problem.lows[0], problem.highs[0]], 
+        #                   ylim=[problem.lows[1], problem.highs[1]])
 
         print(f'Analytic solution {problem.answer}')
 
@@ -39,10 +44,10 @@ if __name__ == '__main__':
         true_values = [problem.exact_integral(cont.mins, cont.maxs) for cont in containers]
         print(f'true value difference {np.sum(true_values) - problem.answer}')
 
-        # signed_errors = np.array(contributions) - np.array(true_values)
+        signed_errors = np.array(contributions) - np.array(true_values)
 
-        # performances = [result['performance'] for result in results]
-        # container_volumes = [cont.volume for cont in containers]
+        performances = [result['performance'] for result in results]
+        container_volumes = [cont.volume for cont in containers]
 
         # if problem.D == 2:
         #     plotContainers(containers, signed_errors, title='Plot of Signed Absolute Errors', 
@@ -53,18 +58,26 @@ if __name__ == '__main__':
         #                 xlim=[-1.0, 1.0], ylim=[-1.0, 1.0], plot_samples=False,
         #                 c_bar_labels='Length scale', integrand=problem.integrand)
         
-        # plt.scatter(performances, signed_errors)
-        # plt.xlabel('R2 score of GP')
-        # plt.ylabel('Signed error')
-        # plt.tight_layout()
-        # plt.savefig(f'figures/gp_errors_{str(problem)}')
-        # plt.close()
-        # print(f'figure saved to figures/gp_errors_{str(problem)}')
+        plt.scatter(performances, signed_errors)
+        plt.xlabel('R2 score of GP')
+        plt.ylabel('Signed error')
+        plt.tight_layout()
+        plt.savefig(f'figures/gp_errors_{str(problem)}')
+        plt.close()
+        print(f'figure saved to figures/gp_errors_{str(problem)}')
 
-        # plt.scatter(container_volumes, signed_errors)
-        # plt.xlabel('Volume')
-        # plt.ylabel('Signed error')
-        # plt.tight_layout()
-        # plt.savefig(f'figures/errors_vs_volumes_{str(problem)}')
-        # plt.close()
-        # print(f'figure saved to figures/errors_vs_volumes_{str(problem)}')
+        plt.scatter(container_volumes, signed_errors)
+        plt.xlabel('Volume')
+        plt.ylabel('Signed error')
+        plt.tight_layout()
+        plt.savefig(f'figures/errors_vs_volumes_{str(problem)}')
+        plt.close()
+        print(f'figure saved to figures/errors_vs_volumes_{str(problem)}')
+
+        plt.scatter(container_volumes, performances)
+        plt.xlabel('Volume')
+        plt.ylabel('R2 score of GP')
+        plt.tight_layout()
+        plt.savefig(f'figures/volumes_vs_r2_{str(problem)}')
+        plt.close()
+        print(f'figure saved to figures/volumes_vs_r2_{str(problem)}')

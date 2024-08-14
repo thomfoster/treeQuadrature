81d6a674fadc4b97774d95e47a7123e24dbe01a3
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2024-08-14 17:03:09 +0100

    made stratifiedSampler more flexible

--- treeQuadrature/samplers/stratifiedSampler.py
+++ treeQuadrature/samplers/stratifiedSampler.py
@@ -5,7 +5,24 @@ import numpy as np
 
 
 class StratifiedSampler(Sampler):
-    def rvs(self, n: int, problem: Problem) -> np.ndarray:
+    def __init__(self, strata_per_dim: int = None, sampling_method: str = 'midpoint'):
+        """
+        Initialize the StratifiedSampler with user-defined parameters.
+
+        Parameters
+        ----------
+        strata_per_dim : int, optional
+            The number of strata (subdivisions) per dimension. If None, the number of strata will be
+            automatically determined based on the number of samples.
+        sampling_method : str, optional
+            The method to use for sampling within each stratum. Options include:
+            - 'midpoint': Sample at the midpoint of each stratum.
+            - 'random': Sample randomly within each stratum.
+        """
+        self.strata_per_dim = strata_per_dim
+        self.sampling_method = sampling_method
+
+    def rvs(self, n: int, problem: Problem, *args, **kwargs) -> np.ndarray:
         """
         Stratified sampling to ensure coverage of the entire domain.
 
@@ -21,16 +38,34 @@ class StratifiedSampler(Sampler):
         np.ndarray
             Samples from the distribution.
         """
-        samples_per_stratum = int(np.ceil(n ** (1/problem.D)))
-        stratified_samples = []
+        # Determine the number of strata per dimension if not provided
+        if self.strata_per_dim is None:
+            strata_per_dim = int(np.ceil(n ** (1/problem.D)))
+        else:
+            strata_per_dim = self.strata_per_dim
         
+        # Calculate the total number of strata
+        total_strata = strata_per_dim ** problem.D
+        if n < total_strata:
+            raise ValueError("Number of samples must be greater than or equal to the total number of strata.")
+
+        # Divide each dimension into strata
+        stratified_samples = []
         for low, high in zip(problem.lows, problem.highs):
-            stratified_intervals = np.linspace(low, high, samples_per_stratum + 1)
-            stratum_samples = (stratified_intervals[:-1] + stratified_intervals[1:]) / 2.0
+            stratified_intervals = np.linspace(low, high, strata_per_dim + 1)
+            if self.sampling_method == 'midpoint':
+                stratum_samples = (stratified_intervals[:-1] + stratified_intervals[1:]) / 2.0
+            elif self.sampling_method == 'random':
+                stratum_samples = stratified_intervals[:-1] + np.random.rand(strata_per_dim) * (stratified_intervals[1:] - stratified_intervals[:-1])
+            else:
+                raise ValueError("Unsupported sampling method. Use 'midpoint' or 'random'.")
             stratified_samples.append(stratum_samples)
         
+        # Create a meshgrid of the strata
         meshgrid = np.meshgrid(*stratified_samples)
         grid_points = np.vstack([mg.flatten() for mg in meshgrid]).T
+        
+        # Select samples randomly from the grid points
         selected_indices = np.random.choice(grid_points.shape[0], n, replace=True)
         
         return grid_points[selected_indices]

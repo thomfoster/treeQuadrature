2ec852b69cdcddbb1c89b8fe0f4786262434082a
thomfoster <36595849+thomfoster@users.noreply.github.com>
2024-08-19 19:18:34 +0100

    add install_requires to setup.py to install requirements during install

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+      
 setup(name='treeQuadrature',
       version='0.1',
       description='Space partition trees for high dimensional integration',
@@ -8,5 +11,6 @@ setup(name='treeQuadrature',
       author_email='thomas.foster@keble.ox.ac.uk',
       license='MIT',
       packages=find_packages(),
+      install_requires=required,
       include_package_data=True,
       zip_safe=False)
